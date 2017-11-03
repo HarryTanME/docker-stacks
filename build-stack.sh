01e7a0f9b986db95f844c68e955441a344fb519e
EC2 Default User <ec2-user@ip-10-0-1-10.us-west-2.compute.internal>
2017-11-03 04:44:47 +0000

    consolidate the dockers

--- build-stack.sh
+++ build-stack.sh
@@ -8,8 +8,8 @@ docker image rm  wodeai/scipy-notebook
 docker image rm  wodeai/r-notebook
 docker image rm  wodeai/tensorflow-notebook
 docker image rm  wodeai/datascience-notebook
-docker image rm  wodeai/pyspark-notebook
-docker image rm  wodeai/all-spark-notebook
+#docker image rm  wodeai/pyspark-notebook
+#docker image rm  wodeai/all-spark-notebook
 
 docker image rm   903030154846.dkr.ecr.us-west-2.amazonaws.com/wodeai-nb:base-notebook
 docker image rm   903030154846.dkr.ecr.us-west-2.amazonaws.com/wodeai-nb:minimal-notebook
@@ -17,8 +17,8 @@ docker image rm   903030154846.dkr.ecr.us-west-2.amazonaws.com/wodeai-nb:scipy-n
 docker image rm   903030154846.dkr.ecr.us-west-2.amazonaws.com/wodeai-nb:r-notebook
 docker image rm   903030154846.dkr.ecr.us-west-2.amazonaws.com/wodeai-nb:tensorflow-notebook
 docker image rm   903030154846.dkr.ecr.us-west-2.amazonaws.com/wodeai-nb:datascience-notebook
-docker image rm   903030154846.dkr.ecr.us-west-2.amazonaws.com/wodeai-nb:pyspark-notebook
-docker image rm   903030154846.dkr.ecr.us-west-2.amazonaws.com/wodeai-nb:all-spark-notebook
+#docker image rm   903030154846.dkr.ecr.us-west-2.amazonaws.com/wodeai-nb:pyspark-notebook
+#docker image rm   903030154846.dkr.ecr.us-west-2.amazonaws.com/wodeai-nb:all-spark-notebook
 
 cd base-notebook
 docker build -t wodeai/base-notebook .
@@ -26,12 +26,12 @@ cd ../minimal-notebook
 docker build -t wodeai/minimal-notebook .
 cd ../scipy-notebook
 docker build -t wodeai/scipy-notebook .
-#cd ../r-notebook
-#docker build -t wodeai/r-notebook .
-#cd ../tensorflow-notebook
-#docker build -t wodeai/tensorflow-notebook .
+cd ../nlp-notebook
+docker build -t wodeai/nlp .
+cd ../tensorflow-notebook
+docker build -t wodeai/deeplearning .
 cd ../datascience-notebook
-docker build -t wodeai/datascience-notebook .
+docker build -t wodeai/datascience .
 #cd ../pyspark-notebook
 #docker build -t wodeai/pyspark-notebook .
 #cd ../all-spark-notebook
@@ -45,18 +45,18 @@ cd ..
 docker tag wodeai/base-notebook 903030154846.dkr.ecr.us-west-2.amazonaws.com/wodeai-nb:base-notebook
 docker tag wodeai/minimal-notebook 903030154846.dkr.ecr.us-west-2.amazonaws.com/wodeai-nb:minimal-notebook
 docker tag wodeai/scipy-notebook 903030154846.dkr.ecr.us-west-2.amazonaws.com/wodeai-nb:scipy-notebook
-docker tag wodeai/r-notebook 903030154846.dkr.ecr.us-west-2.amazonaws.com/wodeai-nb:r-notebook
-#docker tag wodeai/tensorflow-notebook 903030154846.dkr.ecr.us-west-2.amazonaws.com/wodeai-nb:tensorflow-notebook
-docker tag wodeai/datascience-notebook 903030154846.dkr.ecr.us-west-2.amazonaws.com/wodeai-nb:datascience-notebook
+docker tag wodeai/nlp 903030154846.dkr.ecr.us-west-2.amazonaws.com/wodeai-nb:nlp
+docker tag wodeai/deeplearning 903030154846.dkr.ecr.us-west-2.amazonaws.com/wodeai-nb:deeplearning
+docker tag wodeai/datascience 903030154846.dkr.ecr.us-west-2.amazonaws.com/wodeai-nb:datascience
 #docker tag wodeai/pyspark-notebook 903030154846.dkr.ecr.us-west-2.amazonaws.com/wodeai-nb:pyspark-notebook
 #docker tag wodeai/all-spark-notebook 903030154846.dkr.ecr.us-west-2.amazonaws.com/wodeai-nb:all-spark-notebook
 
 docker push  903030154846.dkr.ecr.us-west-2.amazonaws.com/wodeai-nb:base-notebook
 docker push  903030154846.dkr.ecr.us-west-2.amazonaws.com/wodeai-nb:minimal-notebook
 docker push  903030154846.dkr.ecr.us-west-2.amazonaws.com/wodeai-nb:scipy-notebook
-docker push  903030154846.dkr.ecr.us-west-2.amazonaws.com/wodeai-nb:r-notebook
-#docker push  903030154846.dkr.ecr.us-west-2.amazonaws.com/wodeai-nb:tensorflow-notebook
-docker push  903030154846.dkr.ecr.us-west-2.amazonaws.com/wodeai-nb:datascience-notebook
+docker push  903030154846.dkr.ecr.us-west-2.amazonaws.com/wodeai-nb:nlp
+docker push  903030154846.dkr.ecr.us-west-2.amazonaws.com/wodeai-nb:deeplearning
+docker push  903030154846.dkr.ecr.us-west-2.amazonaws.com/wodeai-nb:datascience
 #docker push  903030154846.dkr.ecr.us-west-2.amazonaws.com/wodeai-nb:pyspark-notebook
 #docker push  903030154846.dkr.ecr.us-west-2.amazonaws.com/wodeai-nb:all-spark-notebook
 
